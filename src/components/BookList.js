2939f4ffcd34498510de0a83430f8094168376d8
5windy <5windy@naver.com>
2024-05-14 11:47:25 +0900

    Modify BookList Component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,41 @@
+import {
+  Box,
+  Button,
+  Heading,
+  HStack,
+  Icon,
+  IconButton,
+  Input,
+  Stack,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Tfoot,
+  Th,
+  Thead,
+  Tr,
+  useColorMode,
+  useColorModeValue,
+  VStack,
+} from "@chakra-ui/react";
 import React, { useEffect, useRef, useState } from "react";
+import { MdOndemandVideo } from "react-icons/md";
+import { AiFillSun, AiFillMoon } from "react-icons/ai";
 
 const BookList = () => {
   // useState 는 화면 랜더링에 반영됨
   const [bookList, setBookList] = useState([]);
   const [page, setPage] = useState(1);
-  const [search, setSearch] = useState('달고나 커피');
+  const [search, setSearch] = useState("달고나 커피");
 
   // useRef 는 화면 랜더링 반영되지 않는 참조값
   const pageCount = useRef(1);
+
+  // Chakra UI 에서 제공하는 훅
+  const { colorMode, toggleColorMode } = useColorMode();
+  const color = useColorModeValue("red.500", "white");
+  const buttonScheme = useColorModeValue("blackAlpha", "whiteAlpha");
 
   const fetchBooks = async () => {
     const response = await fetch(
@@ -33,10 +61,9 @@ const BookList = () => {
     setBookList(data.documents);
   };
 
-  const changeSearch = e => {
-    if (e.target.value.length >= 2) 
-        setSearch(e.target.value); 
-  }
+  const changeSearch = (e) => {
+    if (e.target.value.length >= 2) setSearch(e.target.value);
+  };
 
   useEffect(() => {
     fetchBooks();
@@ -44,22 +71,72 @@ const BookList = () => {
 
   return (
     <>
-      <h1>동영상 검색 목록</h1>
-      <input type="text" placeholder="검색어 입력" onChange={changeSearch} />
-      <div>
-        {bookList.map((book) => (
-          <>
-            <p>{book.title}</p>
-          </>
-        ))}
-      </div>
-      <ul>
-        {Array.from({length: pageCount.current}, (_, index) => (
+      <Box>
+        <Heading color={color}>
+          <Icon as={MdOndemandVideo} boxSize={"1.5em"} /> 동영상 검색 목록
+        </Heading>
+
+        {colorMode === "light" ? (
+          <IconButton
+            icon={<AiFillMoon />}
+            onClick={toggleColorMode}
+            size={"lg"}
+          />
+        ) : (
+          <IconButton
+            icon={<AiFillSun />}
+            onClick={toggleColorMode}
+            size={"lg"}
+          />
+        )}
+
+        <Input
+          type="text"
+          placeholder="검색어 입력"
+          onChange={changeSearch}
+          size="lg"
+          variant="filled"
+        />
+        <TableContainer>
+          <Table variant={"striped"} colorScheme="blackAlpha">
+            <Thead>
+              <Tr>
+                <Th>No</Th>
+                <Th>Title</Th>
+                <Th>Author</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {bookList.map((book, index) => (
+                <>
+                  <Tr>
+                    <Td>{(page - 1) * 10 + index + 1}</Td>
+                    <Td>
+                      <a href={book.url}>{book.title}</a>
+                    </Td>
+                    <Td>{book.author}</Td>
+                  </Tr>
+                </>
+              ))}
+            </Tbody>
+            <Tfoot></Tfoot>
+          </Table>
+        </TableContainer>
+        <HStack>
+          {Array.from({ length: pageCount.current }, (_, index) => (
             <>
-                <li onClick={e => { setPage(index + 1) }}>{index + 1}</li>
+              <Button
+                colorScheme={page === index + 1 ? "red" : buttonScheme}
+                onClick={(e) => {
+                  setPage(index + 1);
+                }}
+              >
+                {index + 1}
+              </Button>
             </>
-        ))}
-      </ul>
+          ))}
+        </HStack>
+      </Box>
     </>
   );
 };
