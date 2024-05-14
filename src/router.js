948aa7964a796a3947fa78dc24f88cd2bb8a8371
5windy <5windy@naver.com>
2024-05-14 15:10:18 +0900

    Modify Router

--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "./routes/Root";
-import VideoList from "./components/VideoList";
+import Home from "./components/Home";
 
-// 라우터 설계 
+// 라우터 설계
 /*
 GET     /demo/video                 추천 영상 목록 페이지 
 GET     /demo/video/list            추천 영상 목록 페이지 
@@ -13,34 +13,93 @@ GET     /demo/book/list             추천 도서 목록 페이지
 GET     /demo/book/search           검색 도서 목록 페이지 
 GET     /demo/book/search/{:isbn}   검색 도서 상세 페이지 
 */
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
     {
-        path: "/",
-        element: <Root />,
-        children: [
-            {
-                path: "/sample",
-                element: <>
-                    <p>자식이다.</p>
-                </>
-            }
-        ]
+      path: "/",
+      element: <Root />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+      ],
+      errorElement: <>
+        <h1>Opps!!!</h1>
+      </>
     },
     {
-        path: "/video",
-        element: <Root />,
-        children: [
-        ]
+      path: "/video",
+      element: <Root />,
+      children: [
+        {
+          path: "/video",
+          element: (
+            <>
+              <p>추천 영상 목록 페이지</p>
+            </>
+          ),
+        },
+        {
+          path: "/video/list",
+          element: (
+            <>
+              <p>추천 영상 목록 페이지</p>
+            </>
+          ),
+        },
+        {
+          path: "/video/search",
+          element: (
+            <>
+              <p>영상 검색 목록 페이지</p>
+            </>
+          ),
+        },
+      ],
     },
     {
-        path: "/book",
-        element: <Root />,
-        children: [
-
-        ]
+      path: "/book",
+      element: <Root />,
+      children: [
+        {
+          path: "/book",
+          element: (
+            <>
+              <p>추천 도서 목록 페이지</p>
+            </>
+          ),
+        },
+        {
+          path: "/book/list",
+          element: (
+            <>
+              <p>추천 도서 목록 페이지</p>
+            </>
+          ),
+        },
+        {
+          path: "/book/search",
+          element: (
+            <>
+              <p>도서 검색 목록 페이지</p>
+            </>
+          ),
+        },
+        {
+          path: "/book/search/:isbn",
+          element: (
+            <>
+              <p>검색 도서 상세 페이지</p>
+            </>
+          ),
+        },
+      ],
     },
-], {
-    basename: "/demo"
-});
+  ],
+  {
+    basename: "/demo",
+  }
+);
 
 export default router;
