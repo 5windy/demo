df78ac5921f5d309f494ce9c53ba609c8a765763
5windy <5windy@naver.com>
2024-05-14 16:34:28 +0900

    Remove empty lines

--- src/routes/Footer.js
+++ src/routes/Footer.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import { Box } from "@chakra-ui/react";
+import React from "react";
 
 const Footer = () => {
-    return (
-        <footer>
-            <address>서울시 서초구</address>
-        </footer>
-    );
+  return (
+    <footer>
+        <address>서울시 서초구</address>
+    </footer>
+  );
 };
 
 export default Footer;
