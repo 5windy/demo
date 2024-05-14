28bbdb64fc22eba8699b05ac84b8a6ff95aec628
5windy <5windy@naver.com>
2024-05-14 15:03:32 +0900

    Modufy Home Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-const Home = ({style}) => {
-    // State : Component에 제공하는 상태 값 
-    // ㄴ 화면 랜더링에 영향 O 
-    const [count, setCount] = useState(1);
-    console.log(count);
-
-    const countUp = (e) => {
-        setCount(count + 1);
-    }
-
+const Home = () => {
     return (
-       <>
-            <button style={style} onClick={countUp}>{count}</button>
-       </>
+        <div>
+            MAIN
+        </div>
     );
 };
 
